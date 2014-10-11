003e8b0546a0d510a2f5e42a497178b07ec2d528
samal <samal@treashare.in>
2014-10-11 09:22:52 +0530

    Prepared the Skeleton

--- index.php
+++ index.php
@@ -2,59 +2,78 @@
 /*
 
   PHP Client for Sitback.
-  Send transactional email smoothly.
   
+  Send transactional email with ease.
+
   @author: Samal Gorai
-  
+
   @version: 0.1
-  
+
 */ 
 
 class Sitback
 {
-     const HOST = "http://sitback.smalgorai.com";
-     const RESOURCE = "/api/v1";
-     const ENDPOINT = "/send";
-     private static $instance ;
-     private static $url ;
-    
-    
-     
-     private function __construct($token, $key){
-       //Set all the parameters.
-       $this->generateUrl($token);     
-       //Do all the token validation.
-     }
+    const HOST = "http://sitback.smalgorai.com";
+    const RESOURCE = "/api/v1";
+    const ENDPOINT = "/send";
+    private static $instance ;
+    private static $url ;
 
-     private function generateUrl($token){
-       $this->url = HOST.RESOURCE.SEND."/".$token;     
-       return $url;
-     }
+    private $headers;
 
-     //Singleton instance;    
-     public static function Init($token,$key){
-       if(!self::$instance){
-         self::$instance = new Sitback($token, $key);
-       }
+
+    private function __construct($token, $key){
+        //Set all the parameters.
+        $this->generateUrl($token);     
+        //Do all the token validation.
+    }
+
+    private function generateUrl($token){
+        $this->url = HOST.RESOURCE.SEND."/".$token;     
+    }
+
+    //Singleton instance;    
+    public static function Init($token,$key){
+        if(!self::$instance){
+            self::$instance = new Sitback($token, $key);
+        }
         return self::$instance;
-     }
+    }
 
-    
-     public function send(/*FUll JSON object*/){
-      //send email request to the Sitback.
-     }
-    
-     private function createSignature(){
-       return $this->signature;
-     }
-     
-     private function createConn(){
-       //open connection to Sitback;
-     }
-     public function closeConn(){
-       //close connection
-     }    
-     
+    private function setHeader(){
+        //set headers properly to appliction type json.
+    }
+    public function send($json){
+        //send email request to the Sitback.
+
+        //check sender
+        
+        //check receiver and it is type of Array
+        
+        $this->setHeader();
+
+        //http://php.net/manual/en/function.http-post-data.php
+        //http://php.net/manual/en/http.request.options.php
+
+        if(function_exists('http_post_data') == true) {
+            http_post_data($this->url, $json,$this->header);
+        }else{
+            //report error extension not installed.
+        }
+        //return response;
+    }
+
+    private function createSignature(){
+        return $this->signature;
+    }
+
+    private function createConn(){
+        //open connection to Sitback;
+    }
+    public function closeConn(){
+        //close connection
+    }    
+
 }
 
 ?>
