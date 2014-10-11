c1531248dfd986034f366db6383738d2c63f6361
samal <samal@treashare.in>
2014-10-11 21:18:40 +0530

    using php curl to make post request.

---
+++ Sitback.php
@@ -0,0 +1,104 @@
+<?php
+/*
+
+  PHP Client for Sitback.
+
+  Send transactional email with ease.
+
+  @author: Samal Gorai
+
+  @version: 0.1
+
+*/ 
+
+class SitbackException extends Exception
+{
+}
+
+class Sitback
+{
+    const HOST = "http://sitback.smalgorai.com";
+    const RESOURCE = "/api/v1";
+    const ENDPOINT = "/send";
+    private static $instance ;
+    private static $url ;
+
+    private $headers;
+
+
+    private function __construct($token, $key){
+        //Set all the parameters.
+        $this->generateUrl($token);     
+        //Do all the token validation.
+    }
+
+    private function generateUrl($token){
+        $this->url = HOST.RESOURCE.SEND."/".$token;     
+    }
+
+    //Singleton instance;    
+    public static function Init($token,$key){
+        if(!self::$instance){
+            self::$instance = new Sitback($token, $key);
+        }
+        return self::$instance;
+    }
+
+    private function setHeader(){
+        //set headers properly to appliction type json.
+    }
+    public function send($json){
+        //send email request to the Sitback.
+
+        //check sender
+
+        //check receiver and it is type of Array
+
+        $this->setHeader();
+
+
+        //create a signature
+
+        //json_encode
+
+        $curl = curl_init();
+        if ( $curl === false )
+        {
+            throw new SitbackException('Could not initialise cURL!');
+        }
+
+        curl_setopt( $curl, CURLOPT_URL, $api_url );
+        curl_setopt( $curl, CURLOPT_HTTPHEADER, array ( "Content-Type: application/json" ) );
+        curl_setopt( $curl, CURLOPT_POST, 1 );
+        curl_setopt( $curl, CURLOPT_POSTFIELDS, $json );
+        //More curl opt
+
+        $response = $this->_execute( $curl );
+        return $response;
+    }
+
+    private function _execute( $curl ) {
+        $response = array();
+
+        $response[ 'body' ] = curl_exec( $curl );
+        $response[ 'status' ] = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        curl_close( $ch );
+
+        return $response;
+    }
+
+    private function createSignature(){
+        return $this->signature;
+    }
+
+    private function createConn(){
+        //open connection to Sitback;
+    }
+    public function closeConn(){
+        //close connection
+    }    
+
+}
+
+?>
